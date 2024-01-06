44f8fd7e0cb0e0af9311feb2ed12bf66b74ab0de
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-06 18:48:43 +0300

    add user filter interactivity

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -66,9 +66,13 @@ const Users = () => {
                 {isLoading && <div>Loading...</div>}
                 {isError && <div>{error.message}</div>}
 
-                <UsersFilter />
+                <UsersFilter
+                    onFilterChange={(filterName: string, filterValue: string) => {
+                        console.log(filterName, filterValue);
+                    }}
+                />
 
-                <Table columns={columns} dataSource={users} />
+                <Table columns={columns} dataSource={users} rowKey={'id'} />
             </Space>
         </>
     );
