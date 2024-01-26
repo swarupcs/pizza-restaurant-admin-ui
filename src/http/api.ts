c9657fde5c288d9c4d190195c4fe156bd85f29ef
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-26 21:44:28 +0300

    add tenant form

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,4 +1,4 @@
-import { CreateUserData, Credentials } from '../types';
+import { CreateTenantData, CreateUserData, Credentials } from '../types';
 import { api } from './client';
 
 // Auth service
@@ -8,3 +8,4 @@ export const logout = () => api.post('/auth/logout');
 export const getUsers = (queryString: string) => api.get(`/users?${queryString}`);
 export const getTenants = () => api.get('/tenants');
 export const createUser = (user: CreateUserData) => api.post('/users', user);
+export const createTenant = (tenant: CreateTenantData) => api.post('/tenants', tenant);
