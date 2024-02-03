55d4db2549b6eda351dde8b6f2042624ed032b6b
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-02-03 14:44:27 +0300

    hide restaurants for manger users

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -21,11 +21,7 @@ const getMenuItems = (role: string) => {
             icon: <Icon component={Home} />,
             label: <NavLink to="/">Home</NavLink>,
         },
-        {
-            key: '/restaurants',
-            icon: <Icon component={foodIcon} />,
-            label: <NavLink to="/restaurants">Restaurants</NavLink>,
-        },
+
         {
             key: '/products',
             icon: <Icon component={BasketIcon} />,
@@ -45,6 +41,12 @@ const getMenuItems = (role: string) => {
             icon: <Icon component={UserIcon} />,
             label: <NavLink to="/users">Users</NavLink>,
         });
+        menus.splice(2, 0, {
+            key: '/restaurants',
+            icon: <Icon component={foodIcon} />,
+            label: <NavLink to="/restaurants">Restaurants</NavLink>,
+        });
+
         return menus;
     }
 
