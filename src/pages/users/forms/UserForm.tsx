ae09875399f6a46c255eb99bd0a6931f2a712bf1
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-02-12 14:27:26 +0300

    hide restautant if role admin

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query';
 import { Tenant } from '../../../types';
 
 const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
+    const selectedRole = Form.useWatch('role');
+
     const { data: tenants } = useQuery({
         queryKey: ['tenants'],
         queryFn: () => {
@@ -104,34 +106,35 @@ const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
                                         placeholder="Select role">
                                         <Select.Option value="admin">Admin</Select.Option>
                                         <Select.Option value="manager">Manager</Select.Option>
-                                        <Select.Option value="customer">Customer</Select.Option>
                                     </Select>
                                 </Form.Item>
                             </Col>
-                            <Col span={12}>
-                                <Form.Item
-                                    label="Restaurant"
-                                    name="tenantId"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Restaurant is required',
-                                        },
-                                    ]}>
-                                    <Select
-                                        size="large"
-                                        style={{ width: '100%' }}
-                                        allowClear={true}
-                                        onChange={() => {}}
-                                        placeholder="Select restaurant">
-                                        {tenants?.data.map((tenant: Tenant) => (
-                                            <Select.Option value={tenant.id} key={tenant.id}>
-                                                {tenant.name}
-                                            </Select.Option>
-                                        ))}
-                                    </Select>
-                                </Form.Item>
-                            </Col>
+                            {selectedRole === 'manager' && (
+                                <Col span={12}>
+                                    <Form.Item
+                                        label="Restaurant"
+                                        name="tenantId"
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: 'Restaurant is required',
+                                            },
+                                        ]}>
+                                        <Select
+                                            size="large"
+                                            style={{ width: '100%' }}
+                                            allowClear={true}
+                                            onChange={() => {}}
+                                            placeholder="Select restaurant">
+                                            {tenants?.data.map((tenant: Tenant) => (
+                                                <Select.Option value={tenant.id} key={tenant.id}>
+                                                    {tenant.name}
+                                                </Select.Option>
+                                            ))}
+                                        </Select>
+                                    </Form.Item>
+                                </Col>
+                            )}
                         </Row>
                     </Card>
                 </Space>
