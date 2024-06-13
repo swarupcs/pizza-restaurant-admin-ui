9ae23787813baa44da520331ee4a2c4c73cc2319
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-06-13 13:49:33 +0300

    add breadcrumb to single order page

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ import Users from './pages/users/Users';
 import Tenants from './pages/tenants/Tenants';
 import Products from './pages/products/Products';
 import Orders from './pages/orders/Orders';
+import SingleOrder from './pages/orders/SingleOrder';
 
 export const router = createBrowserRouter([
     {
@@ -37,6 +38,10 @@ export const router = createBrowserRouter([
                     {
                         path: '/orders',
                         element: <Orders />,
+                    },
+                    {
+                        path: '/orders/:orderId',
+                        element: <SingleOrder />,
                     },
                 ],
             },
