389861796a7262b37549112f2f5cbe6ca5da48b3
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-06-18 14:30:29 +0300

    setup websocket

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -7,6 +7,9 @@ import { getOrders } from '../../http/api';
 import { format } from 'date-fns';
 import { colorMapping } from '../../constants';
 import { capitalizeFirst } from '../products/helpers';
+import React from 'react';
+import socket from '../../lib/socket';
+import { useAuthStore } from '../../store';
 
 const columns = [
     {
@@ -96,6 +99,29 @@ const columns = [
 const TENANT_ID = 10;
 
 const Orders = () => {
+    const { user } = useAuthStore();
+
+    React.useEffect(() => {
+        if (user?.tenant) {
+            socket.on('order-update', (data) => {
+                console.log('data received: ', data);
+            });
+
+            socket.on('join', (data) => {
+                console.log('User joined in: ', data.roomId);
+            });
+
+            socket.emit('join', {
+                tenantId: user.tenant.id,
+            });
+        }
+
+        return () => {
+            socket.off('join');
+            socket.off('order-update');
+        };
+    }, []);
+
     const { data: orders } = useQuery({
         queryKey: ['orders'],
         queryFn: () => {
