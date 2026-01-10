abc8dc54c099208195bc192c99e627b8830945e7
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-01-10 10:29:59 +0000

    add login endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,24 @@ import { ConfigProvider } from 'antd';
 import ReactDOM from 'react-dom/client';
 import 'antd/dist/reset.css';
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorPrimary: '#F65F42',
-                    colorLink: '#F65F42',
-                },
-            }}>
-            <RouterProvider router={router} />
-        </ConfigProvider>
+        <QueryClientProvider client={queryClient}>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorPrimary: '#F65F42',
+                        colorLink: '#F65F42',
+                    },
+                }}>
+                <RouterProvider router={router} />
+            </ConfigProvider>
+        </QueryClientProvider>
     </React.StrictMode>
 );
