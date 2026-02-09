5092bf12fc8d0411fef029f60b11ca3e9b8187a4
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-02-09 15:17:59 +0000

    add users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,39 @@
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Space, Table } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { getUsers } from '../../http/api';
 import { User } from '../../types';
+
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'firstName',
+        key: 'firstName',
+        render: (_text: string, record: User) => {
+            return (
+                <div>
+                    {record.firstName} {record.lastName}
+                </div>
+            );
+        },
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
+    },
+];
 
 const Users = () => {
     const {
@@ -20,21 +50,16 @@ const Users = () => {
 
     return (
         <>
-            <Breadcrumb
-                separator={<RightOutlined />}
-                items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Users' }]}
-            />
-            {isLoading && <div>Loading...</div>}
-            {isError && <div>{error.message}</div>}
-            {users && (
-                <div>
-                    <ul>
-                        {users.map((user: User) => (
-                            <li key={user.id}>{user.firstName}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            <Space direction="vertical" size="large" style={{ width: '100%' }}>
+                <Breadcrumb
+                    separator={<RightOutlined />}
+                    items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Users' }]}
+                />
+                {isLoading && <div>Loading...</div>}
+                {isError && <div>{error.message}</div>}
+
+                <Table columns={columns} dataSource={users} />
+            </Space>
         </>
     );
 };
