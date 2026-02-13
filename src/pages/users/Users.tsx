e59f388772c5262cba001138b906a3c848ea5625
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-02-13 09:53:59 +0000

    users filter design

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -5,6 +5,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getUsers } from '../../http/api';
 import { User } from '../../types';
 import { useAuthStore } from '../../store';
+import UsersFilter from './UsersFilter';
 
 const columns = [
     {
@@ -64,6 +65,8 @@ const Users = () => {
                 />
                 {isLoading && <div>Loading...</div>}
                 {isError && <div>{error.message}</div>}
+
+                <UsersFilter />
 
                 <Table columns={columns} dataSource={users} />
             </Space>
