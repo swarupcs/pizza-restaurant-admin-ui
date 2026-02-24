8d032e327c81ed88d3dd35d70db1f5dc3cf26714
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-02-24 17:41:59 +0000

    fetch tenants for select

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -1,23 +1,85 @@
-import { Card, Col, Form, Input, Row } from 'antd';
+import { Card, Col, Form, Input, Row, Select, Space } from 'antd';
+import { getTenants } from '../../../http/api';
+import { useQuery } from '@tanstack/react-query';
+import { Tenant } from '../../../types';
 
 const UserForm = () => {
+    const { data: tenants } = useQuery({
+        queryKey: ['tenants'],
+        queryFn: () => {
+            return getTenants().then((res) => res.data);
+        },
+    });
+
     return (
         <Row>
             <Col span={24}>
-                <Card title="Basic info" bordered={false}>
-                    <Row gutter={20}>
-                        <Col span={12}>
-                            <Form.Item label="First name" name="firstName">
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col span={12}>
-                            <Form.Item label="Last name" name="lastName">
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                </Card>
+                <Space direction="vertical" size="large">
+                    <Card title="Basic info" bordered={false}>
+                        <Row gutter={20}>
+                            <Col span={12}>
+                                <Form.Item label="First name" name="firstName">
+                                    <Input size="large" />
+                                </Form.Item>
+                            </Col>
+                            <Col span={12}>
+                                <Form.Item label="Last name" name="lastName">
+                                    <Input size="large" />
+                                </Form.Item>
+                            </Col>
+                            <Col span={12}>
+                                <Form.Item label="Email" name="email">
+                                    <Input size="large" />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Card>
+
+                    <Card title="Security info" bordered={false}>
+                        <Row gutter={20}>
+                            <Col span={12}>
+                                <Form.Item label="Passoword" name="password">
+                                    <Input size="large" type="password" />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Card>
+
+                    <Card title="Role" bordered={false}>
+                        <Row gutter={20}>
+                            <Col span={12}>
+                                <Form.Item label="Role" name="role">
+                                    <Select
+                                        size="large"
+                                        style={{ width: '100%' }}
+                                        allowClear={true}
+                                        onChange={() => {}}
+                                        placeholder="Select role">
+                                        <Select.Option value="admin">Admin</Select.Option>
+                                        <Select.Option value="manager">Manager</Select.Option>
+                                        <Select.Option value="customer">Customer</Select.Option>
+                                    </Select>
+                                </Form.Item>
+                            </Col>
+                            <Col span={12}>
+                                <Form.Item label="Restaurant" name="tenantId">
+                                    <Select
+                                        size="large"
+                                        style={{ width: '100%' }}
+                                        allowClear={true}
+                                        onChange={() => {}}
+                                        placeholder="Select restaurant">
+                                        {tenants?.map((tenant: Tenant) => (
+                                            <Select.Option value={tenant.id}>
+                                                {tenant.name}
+                                            </Select.Option>
+                                        ))}
+                                    </Select>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Space>
             </Col>
         </Row>
     );
