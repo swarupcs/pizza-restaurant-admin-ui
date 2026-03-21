beb192479ffe730533ca5ce041d549d87d44e6b1
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-03-21 06:35:59 +0000

    add tenant loading spinner

--- src/pages/tenants/Tenants.tsx
+++ src/pages/tenants/Tenants.tsx
@@ -1,5 +1,16 @@
-import { Breadcrumb, Button, Drawer, Form, Space, Table, theme } from 'antd';
-import { RightOutlined, PlusOutlined } from '@ant-design/icons';
+import {
+    Breadcrumb,
+    Button,
+    Drawer,
+    Flex,
+    Form,
+    Space,
+    Spin,
+    Table,
+    Typography,
+    theme,
+} from 'antd';
+import { RightOutlined, PlusOutlined, LoadingOutlined } from '@ant-design/icons';
 import { Link, Navigate } from 'react-router-dom';
 import { keepPreviousData, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useAuthStore } from '../../store';
@@ -45,7 +56,7 @@ const Tenants = () => {
     const [drawerOpen, setDrawerOpen] = React.useState(false);
     const {
         data: tenants,
-        isLoading,
+        isFetching,
         isError,
         error,
     } = useQuery({
@@ -110,12 +121,16 @@ const Tenants = () => {
     return (
         <>
             <Space direction="vertical" size="large" style={{ width: '100%' }}>
-                <Breadcrumb
-                    separator={<RightOutlined />}
-                    items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Tenants' }]}
-                />
-                {isLoading && <div>Loading...</div>}
-                {isError && <div>{error.message}</div>}
+                <Flex justify="space-between">
+                    <Breadcrumb
+                        separator={<RightOutlined />}
+                        items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Tenants' }]}
+                    />
+                    {isFetching && (
+                        <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+                    )}
+                    {isError && <Typography.Text type="danger">{error.message}</Typography.Text>}
+                </Flex>
 
                 <Form form={filterForm} onFieldsChange={onFilterChange}>
                     <TenantFilter>
