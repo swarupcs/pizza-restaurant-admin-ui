09b360b97bba728e90ea4ff90775fd4ae6ac73a6
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-04-12 22:11:59 +0000

    fetch categories and restaurants for select box

--- src/http/api.ts
+++ src/http/api.ts
@@ -19,3 +19,4 @@ export const updateUser = (user: CreateUserData, id: string) =>
     api.patch(`${AUTH_SERVICE}/users/${id}`, user);
 
 // Catelog service
+export const getCategories = () => api.get(`${CATALOG_SERVICE}/categories`);
