e4a7d08ab94792147634b07b05ea56db8594d869
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-04-28 00:35:59 +0000

    create product functionality

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -18,12 +18,13 @@ import ProductsFilter from './ProductsFilter';
 import { FieldData, Product } from '../../types';
 import React from 'react';
 import { PER_PAGE } from '../../constants';
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
-import { getProducts } from '../../http/api';
+import { keepPreviousData, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createProduct, getProducts } from '../../http/api';
 import { format } from 'date-fns';
 import { debounce } from 'lodash';
 import { useAuthStore } from '../../store';
 import ProductForm from './forms/ProductForm';
+import { makeFormData } from './helpers';
 
 const columns = [
     {
@@ -127,9 +128,79 @@ const Products = () => {
             setQueryParams((prev) => ({ ...prev, ...changedFilterFields, page: 1 }));
         }
     };
+    const queryClient = useQueryClient();
+    const { mutate: productMutate } = useMutation({
+        mutationKey: ['product'],
+        mutationFn: async (data: FormData) => createProduct(data).then((res) => res.data),
+        onSuccess: async () => {
+            queryClient.invalidateQueries({ queryKey: ['products'] });
+            form.resetFields();
+            setDrawerOpen(false);
+            return;
+        },
+    });
 
-    const onHandleSubmit = () => {
-        console.log('submitting...');
+    const onHandleSubmit = async () => {
+        // const dummy = {
+        //     Size: { priceType: 'base', availableOptions: { Small: 400, Medium: 600, Large: 800 } },
+        //     Crust: { priceType: 'aditional', availableOptions: { Thin: 50, Thick: 100 } },
+        // };
+
+        // const currentData = {
+        //     '{"configurationKey":"Size","priceType":"base"}': {
+        //         Small: 100,
+        //         Medium: 200,
+        //         Large: 400,
+        //     },
+        //     '{"configurationKey":"Crust","priceType":"aditional"}': {
+        //         Thin: 0,
+        //         Thick: 50,
+        //     },
+        // };
+
+        await form.validateFields();
+
+        const priceConfiguration = form.getFieldValue('priceConfiguration');
+        const pricing = Object.entries(priceConfiguration).reduce((acc, [key, value]) => {
+            const parsedKey = JSON.parse(key);
+            return {
+                ...acc,
+                [parsedKey.configurationKey]: {
+                    priceType: parsedKey.priceType,
+                    availableOptions: value,
+                },
+            };
+        }, {});
+
+        const categoryId = JSON.parse(form.getFieldValue('categoryId'))._id;
+        // const currentAttrs = {
+        //     isHit: 'No',
+        //     Spiciness: 'Less',
+        // };
+
+        // const attrs = [
+        //     { name: 'Is Hit', value: true },
+        //     { name: 'Spiciness', value: 'Hot' },
+        // ];
+
+        const attributes = Object.entries(form.getFieldValue('attributes')).map(([key, value]) => {
+            return {
+                name: key,
+                value: value,
+            };
+        });
+
+        const postData = {
+            ...form.getFieldsValue(),
+            isPublish: form.getFieldValue('isPublish') ? true : false,
+            image: form.getFieldValue('image'),
+            categoryId,
+            priceConfiguration: pricing,
+            attributes,
+        };
+
+        const formData = makeFormData(postData);
+        await productMutate(formData);
     };
 
     return (
--- src/pages/products/forms/ProductForm.tsx
+++ src/pages/products/forms/ProductForm.tsx
@@ -1,12 +1,30 @@
-import { Card, Col, Form, Input, Row, Select, Space, Switch, Typography, Upload } from 'antd';
+import {
+    Card,
+    Col,
+    Form,
+    Input,
+    message,
+    Row,
+    Select,
+    Space,
+    Switch,
+    Typography,
+    Upload,
+    UploadProps,
+} from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { Category, Tenant } from '../../../types';
 import { useQuery } from '@tanstack/react-query';
 import { getCategories, getTenants } from '../../../http/api';
 import Pricing from './Pricing';
 import Attributes from './Attributes';
+import { useState } from 'react';
+import { set } from 'lodash';
 
 const ProductForm = () => {
+    const [messageApi, contextHolder] = message.useMessage();
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
+
     const selectedCategory = Form.useWatch('categoryId');
     console.log(selectedCategory);
     const { data: categories } = useQuery({
@@ -22,6 +40,25 @@ const ProductForm = () => {
             return getTenants(`perPage=100&currentPage=1`);
         },
     });
+
+    const uploaderConfig: UploadProps = {
+        name: 'file',
+        multiple: false,
+        showUploadList: false,
+        beforeUpload: (file) => {
+            // Validation logic
+            const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+            if (!isJpgOrPng) {
+                console.error('You can only upload JPG/PNG file!');
+                messageApi.error('You can only upload JPG/PNG file!');
+            }
+
+            //todo:  size validation.
+            setImageUrl(URL.createObjectURL(file));
+
+            return false;
+        },
+    };
 
     return (
         <Row>
@@ -100,11 +137,20 @@ const ProductForm = () => {
                                             message: 'Please upload a product image',
                                         },
                                     ]}>
-                                    <Upload listType="picture-card">
-                                        <Space direction="vertical">
-                                            <PlusOutlined />
-                                            <Typography.Text>Upload</Typography.Text>
-                                        </Space>
+                                    {contextHolder}
+                                    <Upload listType="picture-card" {...uploaderConfig}>
+                                        {imageUrl ? (
+                                            <img
+                                                src={imageUrl}
+                                                alt="avatar"
+                                                style={{ width: '100%' }}
+                                            />
+                                        ) : (
+                                            <Space direction="vertical">
+                                                <PlusOutlined />
+                                                <Typography.Text>Upload</Typography.Text>
+                                            </Space>
+                                        )}
                                     </Upload>
                                 </Form.Item>
                             </Col>
