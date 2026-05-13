142e1a074c24fdfd2237277a9aa66ce83bebc23c
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2026-05-13 02:59:59 +0000

    render order cart items list

--- src/pages/orders/SingleOrder.tsx
+++ src/pages/orders/SingleOrder.tsx
@@ -1,8 +1,30 @@
-import { Breadcrumb, Flex, Space } from 'antd';
+import { Avatar, Breadcrumb, Card, Col, Flex, List, Row, Space, Tag, Typography } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { colorMapping } from '../../constants';
+import { capitalizeFirst } from '../products/helpers';
+import { useQuery } from '@tanstack/react-query';
+import { getSingle } from '../../http/api';
+import { Order } from '../../types';
 
 const SingleOrder = () => {
+    const params = useParams();
+    const orderId = params.orderId;
+
+    const { data: order } = useQuery<Order>({
+        queryKey: ['order', orderId],
+        queryFn: () => {
+            const queryString = new URLSearchParams({
+                fields: 'cart,address,paymentMode,tenantId,total,comment,orderStatus,paymentStatus',
+            }).toString();
+            return getSingle(orderId as string, queryString).then((res) => res.data);
+        },
+    });
+
+    if (!order) {
+        return null;
+    }
+    console.log('order', order);
     return (
         <Space direction="vertical" size="large" style={{ width: '100%' }}>
             <Flex justify="space-between">
@@ -11,10 +33,57 @@ const SingleOrder = () => {
                     items={[
                         { title: <Link to="/">Dashboard</Link> },
                         { title: <Link to="/orders">Orders</Link> },
-                        { title: `Order #123445554534545` },
+                        { title: `Order #${order?._id}` },
                     ]}
                 />
             </Flex>
+
+            <Row gutter={24}>
+                <Col span={14}>
+                    <Card
+                        title="Order Details"
+                        extra={
+                            <Tag
+                                bordered={false}
+                                color={colorMapping[order.orderStatus] ?? 'processing'}>
+                                {capitalizeFirst(order.orderStatus)}
+                            </Tag>
+                        }>
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={order.cart}
+                            renderItem={(item, index) => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar src={item.image} />}
+                                        title={item.name}
+                                        description={item.chosenConfiguration.selectedToppings[0]
+                                            // todo: IMPORTANT: check why there is a nested array in selected toppings
+                                            // @ts-ignore
+                                            .map((topping) => topping.name)
+                                            .join(', ')}
+                                    />
+
+                                    <Space size={'large'}>
+                                        <Typography.Text>
+                                            {Object.values(
+                                                item.chosenConfiguration.priceConfiguration
+                                            ).join(', ')}
+                                        </Typography.Text>
+
+                                        <Typography.Text>
+                                            {item.qty} Item{item.qty > 1 ? 's' : ''}
+                                        </Typography.Text>
+                                    </Space>
+                                </List.Item>
+                            )}
+                        />
+                    </Card>
+                </Col>
+                <Col span={10}>
+                    <Card title="Customer Details">some content goes here...</Card>
+                </Col>
+            </Row>
         </Space>
     );
 };
